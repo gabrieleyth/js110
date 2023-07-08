2c493c98a9c0702ece572c5b94393deda72f2fe6
gabrieleyth <gabriel.leyth@outlook.com>
2023-07-08 09:47:08 +0000

    test function that doubles odd numbers

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
-let numbers = [1, 2, 3, 4];
-
-for (let i=0; i < numbers.length; i++) {
-  numbers[i] = numbers[i] + 1;
+function doubleOddNumbers(numbers) {  
+  let newNumbers = [];
+  for (let i=0; i < numbers.length; i++) {
+    if (i % 2 !==0) {
+      let doubledOddNumber = numbers[i] * 2;
+      newNumbers.push(doubledOddNumber); 
+    } else {
+      newNumbers.push(numbers[i]);
+    }
+  }
+  return newNumbers;
 }
 
-console.log(numbers); // [ 2, 3, 4, 5 ]
+let myNumbers = [1, 4, 3, 7, 2, 6];
+console.log(doubleOddNumbers(myNumbers)); // [ 1, 8, 3, 14, 2, 12 ]
+console.log(myNumbers); // [ 1, 4, 3, 7, 2, 6 ]
