9d650ec0d0565f29335a7e6108afe56d81dd9a15
gabrieleyth <gabriel.leyth@outlook.com>
2023-07-12 15:46:16 +0000

    The time of day can be represented as the number of minutes before or after midnight. If the number of minutes is positive, the time is after midnight. If the number of minutes is negative, the time is before midnight.

--- aftermidnight.js
+++ aftermidnight.js
@@ -10,23 +10,31 @@ const MINUTES_PER_HOUR = 60;
 const HOURS_PER_DAY = 24;
 const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
 
+// Function to add a leading zero to single-digit numbers
 function leadingZero(number) {
   return number < 10 ? `0${number}` : String(number);
 }
 
+// Function to format the time in "hh:mm" format
 function formatTime(hours, minutes) {
   return `${leadingZero(hours)}:${leadingZero(minutes)}`;
 }
 
+// Function to calculate the time of day based on the deltaMinutes
 function timeOfDay(deltaMinutes) {
+  // Check if deltaMinutes is negative
   if (deltaMinutes < 0) {
+    // If negative, convert deltaMinutes to a positive value within the range of a day
     deltaMinutes = (deltaMinutes % MINUTES_PER_DAY) + MINUTES_PER_DAY;
   } else {
+    // If positive, calculate deltaMinutes within the range of a day
     deltaMinutes = deltaMinutes % MINUTES_PER_DAY;
   }
 
+  // Calculate the hours and minutes from deltaMinutes
   let hours = Math.floor(deltaMinutes / MINUTES_PER_HOUR);
   let minutes = deltaMinutes % MINUTES_PER_HOUR;
 
+  // Return the formatted time
   return formatTime(hours, minutes);
 }
