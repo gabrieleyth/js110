0d9c5b2af75c76f66beb80e2aa68baadadb10d2c
gabrieleyth <gabriel.leyth@outlook.com>
2023-07-12 16:12:10 +0000

    two functions that each take a time of day in 24 hour format, and return the number of minutes before and after midnight, respectively.

--- aftermidnight.js
+++ aftermidnight.js
@@ -1,40 +1,25 @@
-console.log(timeOfDay(0) === "00:00");
-console.log(timeOfDay(-3) === "23:57");
-console.log(timeOfDay(35) === "00:35");
-console.log(timeOfDay(-1437) === "00:03");
-console.log(timeOfDay(3000) === "02:00");
-console.log(timeOfDay(800) === "13:20");
-console.log(timeOfDay(-4231) === "01:29");
+console.log(afterMidnight("00:00") === 0);
+console.log(beforeMidnight("00:00") === 0);
+console.log(afterMidnight("12:34") === 754);
+console.log(beforeMidnight("12:34") === 686);
+console.log(afterMidnight("24:00") === 0);
+console.log(beforeMidnight("24:00") === 0);
 
-const MINUTES_PER_HOUR = 60;
-const HOURS_PER_DAY = 24;
-const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
-
-// Function to add a leading zero to single-digit numbers
-function leadingZero(number) {
-  return number < 10 ? `0${number}` : String(number);
-}
-
-// Function to format the time in "hh:mm" format
-function formatTime(hours, minutes) {
-  return `${leadingZero(hours)}:${leadingZero(minutes)}`;
-}
-
-// Function to calculate the time of day based on the deltaMinutes
-function timeOfDay(deltaMinutes) {
-  // Check if deltaMinutes is negative
-  if (deltaMinutes < 0) {
-    // If negative, convert deltaMinutes to a positive value within the range of a day
-    deltaMinutes = (deltaMinutes % MINUTES_PER_DAY) + MINUTES_PER_DAY;
+function afterMidnight(input) {
+  let arrTime = input.split(":");
+  if (Number(arrTime[0]) > 0 && Number(arrTime[0]) < 24) {
+   let minutesAfterMidnight = Math.floor(Number(arrTime[0])*60 + Number(arrTime[1]));
+   return minutesAfterMidnight;
   } else {
-    // If positive, calculate deltaMinutes within the range of a day
-    deltaMinutes = deltaMinutes % MINUTES_PER_DAY;
+    return 0;
   }
+}
 
-  // Calculate the hours and minutes from deltaMinutes
-  let hours = Math.floor(deltaMinutes / MINUTES_PER_HOUR);
-  let minutes = deltaMinutes % MINUTES_PER_HOUR;
-
-  // Return the formatted time
-  return formatTime(hours, minutes);
+function beforeMidnight(input) {
+  let arrTime = input.split(":");
+  if (Number(arrTime[0]) > 0 && Number(arrTime[0]) < 24) {
+    return (1440 - afterMidnight(input));
+  } else {
+    return 0;
+  }
 }
