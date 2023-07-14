b6cd12e3530bb7d97c843538221a02623c69789f
gabrieleyth <gabriel.leyth@outlook.com>
2023-07-14 09:18:52 +0000

    reasigning 3 to 4 and accessing gs in colorful arrays and objects

--- accessgs.js
+++ accessgs.js
@@ -1,3 +1,22 @@
+// Reasigning 3 to 4
+let arr1 = [1, [2, 3], 4];
+arr1[1][1] = 4;
+console.log(arr1);
+
+let arr2 = [{ a: 1 }, { b: 2, c: [7, 6, 5], d: 4 }, 3];
+arr2[2] = 4;
+console.log(arr2);
+
+let obj1 = { first: [1, 2, [3]] };
+obj1.first[2][0] = 4;
+console.log(obj1);
+
+let obj2 = { a: { a: ['1', 'two', 3], b: 4 }, b: 5 };
+obj2.a.a[2] = 4;
+console.log(obj2);
+
+
+// Accessing G
 let arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]];
 console.log(arr1[2][1][3]); // g
 
