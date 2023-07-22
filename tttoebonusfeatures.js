60d184fe391723d3135a0a58fb2e7491e983956c
gabrieleyth <gabriel.leyth@outlook.com>
2023-07-22 10:34:44 +0000

    TicTacToe game with bonus features joinOr, keepingScore, and playAgainHandling

--- tttoebonusfeatures.js
+++ tttoebonusfeatures.js
@@ -46,10 +46,10 @@ function emptySquares(board) {
 function joinOr(input, separator, last) {
   // || means if there is no separator/last it is a falsy value, the value will thus be the first truthy value
   // which i set as default "," and "or"
-  
+
   if (input.length > 2) {
-  let newArr = [];
-  
+    let newArr = [];
+
     for (let index = 0; index < input.length; index++) {
       if (index < input.length - 1) {
         newArr.push(input[index]);
@@ -57,17 +57,17 @@ function joinOr(input, separator, last) {
       } else {
         newArr.push((last || "or") + " " + input[index]);
         // push into the array the last element preceeded by custom or default last word and a mandatory space
-        // otherwise you get 1, 2, and3 
+        // otherwise you get 1, 2, and3
       }
     }
-  
-  prompt(`Choose a square: ${newArr.join(separator || ", ")}`);
-  // combine everything based on a custom or default separator
-   
+
+    prompt(`Choose a square: ${newArr.join(separator || ", ")}`);
+    // combine everything based on a custom or default separator
+
   } else if ( input.length === 2) {
     prompt(`Choose a square: ${input[0]} ${last || "or"} ${input[1]}`);
   } else {
-    prompt(`Choose a square: ${input[0] || ""}`); 
+    prompt(`Choose a square: ${input[0] || ""}`);
     // prevents empty arrays from logging undefined and logs empty string instead
   }
 }
@@ -102,7 +102,7 @@ function someoneWon(board) {
 }
 
 function detectWinner(board) {
- 
+
   let winningLines = [
     [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
     [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
@@ -113,14 +113,14 @@ function detectWinner(board) {
     let [ sq1, sq2, sq3 ] = winningLines[line];
 
     if (
-        board[sq1] === HUMAN_MARKER &&
+      board[sq1] === HUMAN_MARKER &&
         board[sq2] === HUMAN_MARKER &&
         board[sq3] === HUMAN_MARKER
     ) {
       return 'Player';
-      
+
     } else if (
-        board[sq1] === COMPUTER_MARKER &&
+      board[sq1] === COMPUTER_MARKER &&
         board[sq2] === COMPUTER_MARKER &&
         board[sq3] === COMPUTER_MARKER
     ) {
@@ -134,7 +134,7 @@ function detectWinner(board) {
 // keepscore bonus features modifications start
 let playerScore = 0;
 let computerScore = 0;
-  
+
 function scoreKeeper(board) {
   if (board === "Player") {
     playerScore += 1;
@@ -181,7 +181,7 @@ gameLoop: while (true) {
   playAgainLoop: while (true) {
     prompt('Play again?');
     let answer = readline.question().toLowerCase();
-  
+
     if (answer === "y") {
       break playAgainLoop; // breaks out of this loop but overall loop remains true
     } else if (answer === 'n') {
